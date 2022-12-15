46c9c525309abbb3f4c889369025c1b7c280c065
imjays <jpkumar541@gmail.com>
2022-12-15 11:45:23 +0530

    Add project files.

---
+++ Day4Interface2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Interface2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(5, 10);
+            Complex b = new Complex(15, 20);
+            Complex c;
+            c = a + b;
+            Complex.Print(a);
+            Complex.Print(b);
+            Complex.Print(c);
+        }
+    }
+
+    class Complex
+    {
+        int _x, _y;
+        public Complex() { }
+        public Complex(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public static Complex operator +(Complex op1, Complex op2)
+        {
+            Complex result = null;
+            result = new Complex();
+            result._x = op1._x + op2._x;
+            result._y = op1._y + op2._y;
+            return result;
+        }
+        public static Complex operator -(Complex op1)
+        {
+            Complex result=null;
+            result = new Complex();
+            result._x-= op1._x;
+            result._y-= op1._y;
+            return result;
+        }
+        public static void Print(Complex p)
+        {
+            Console.WriteLine("x:{0} y:{1}", p._x, p._y);
+        }
+    }
+}
